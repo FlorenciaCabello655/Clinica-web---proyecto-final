7e6cd3cc5961ec8c00b102189fb26ba4795e5a8c
Florencia Cabello <florenciaacabello@gmail.com>
2026-02-07 15:39:58 -0300

    haciendo responsive el inicio

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -4,26 +4,26 @@ import doctora from "../../assets/doctora_banner.png";
 function Banner() {
   return (
     <>
-      <div className="bg-[#DCCFC0] w-full h-150 flex justify-between items-center ps-10">
-        <div className="max-w-[55%]">
-          <h1 className="text-7xl text-[#A2AF9B] ">
+      <div className="bg-[#DCCFC0] w-full lg:h-150 flex justify-between items-center p-4 md:ps-10">
+        <div className="md:max-w-[50%] lg:max-w-[55%]">
+          <h1 className="text-center md:text-start text-3xl md:text-5xl lg:text-7xl text-[#A2AF9B] ">
             Bienvenidos a Clinica Web
           </h1>
-          <h2 className="text-xl text-white">
+          <h2 className="text-center md:text-start text-base md:text-xl text-white">
             Brindamos atención médica integral con un enfoque humano,
             profesional y cercano. Nuestro equipo de especialistas trabaja con
             compromiso y tecnología actual para cuidar tu salud y la de tu
             familia, priorizando siempre la confianza, la calidez y el
             bienestar.
           </h2>
-          <div>
+          <div className="flex w-full justify-center md:justify-start mt-3">
             <Link className="p-2 rounded-full font-bold text-white duration-200 bg-[#A2AF9B]">
               Solicitar turno
             </Link>
           </div>
         </div>
         <div>
-          <img src={doctora} alt="" className="h-150 w-auto" />
+          <img src={doctora} alt="" className="hidden md:block md:h-100 lg:h-150 w-auto " />
         </div>
       </div>
     </>
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,63 +6,56 @@ import Dermatologia from "../../assets/dermatologia.png";
 import Oftalmologia from "../../assets/oftalmologia.png";
 import Traumatologia from "../../assets/traumatologia.png";
 import Imagen from "../../assets/clinicaimg.png";
-import './home.css'
+import "./home.css";
 
 function Home() {
   return (
     <>
       <Banner></Banner>
-      <section className=" flex justify-around mt-3">
-        <div className=" bg-[#A2AF9B] h-60 w-70 text-4xl text-white flex items-center justify-center rounded-lg flex-col">
-          {" "}
-          <img src={Cardiologia} alt="" className=" w-40 " />
-          <h1>Cardiologia</h1>
+      <section className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-3 px-5 mt-3">
+        <div className=" bg-[#A2AF9B] h-60 w-full text-white flex items-center justify-center rounded-lg flex-col">
+          <img src={Cardiologia} alt="" className="w-30 md:w-32 lg:w-40 " />
+          <h1 className="text-lg md:text-2xl lg:text-3xl">Cardiologia</h1>
         </div>
-        <div className=" bg-[#A2AF9B] h-60 w-70 text-4xl text-white flex items-center justify-center rounded-lg flex-col">
-          {" "}
-          <img src={Clinica} alt="" className=" w-40 " />{" "}
-          <h1>Clinica General</h1>
+        <div className=" bg-[#A2AF9B] h-60 w-full text-white flex items-center justify-center rounded-lg flex-col">
+          <img src={Clinica} alt="" className="w-30 md:w-32 lg:w-40 " />
+          <h1 className="text-lg md:text-2xl lg:text-3xl">Clinica General</h1>
         </div>
-        <div className=" bg-[#A2AF9B] h-60 w-70 text-4xl text-white flex items-center justify-center rounded-lg flex-col">
-          {" "}
-          <img src={Pediatria} alt="" className=" w-40 " /> <h1>Pediatria</h1>
+        <div className=" bg-[#A2AF9B] h-60 w-full text-white flex items-center justify-center rounded-lg flex-col">
+          <img src={Pediatria} alt="" className="w-30 md:w-32 lg:w-40 " />
+          <h1 className="text-lg md:text-2xl lg:text-3xl">Pediatria</h1>
         </div>
-        <div className=" bg-[#A2AF9B] h-60 w-70 text-4xl text-white flex items-center justify-center rounded-lg flex-col">
-          {" "}
-          <img src={Traumatologia} alt="" className=" w-40 " />{" "}
-          <h1>Traumatologia</h1>
+        <div className=" bg-[#A2AF9B] h-60 w-full text-white flex items-center justify-center rounded-lg flex-col">
+          <img src={Traumatologia} alt="" className="w-30 md:w-32 lg:w-40 " />
+          <h1 className="text-lg md:text-2xl lg:text-3xl">Traumatologia</h1>
         </div>
-        <div className=" bg-[#A2AF9B] h-60 w-70 text-4xl text-white flex items-center justify-center rounded-lg flex-col">
-          {" "}
-          <img src={Dermatologia} alt="" className=" w-40 " />{" "}
-          <h1>Dermatologia</h1>
+        <div className=" bg-[#A2AF9B] h-60 w-full text-white flex items-center justify-center rounded-lg flex-col">
+          <img src={Dermatologia} alt="" className="w-30 md:w-32 lg:w-40 " />
+          <h1 className="text-lg md:text-2xl lg:text-3xl">Dermatologia</h1>
         </div>
-        <div className=" bg-[#A2AF9B] h-60 w-70 text-4xl text-white flex items-center justify-center rounded-lg flex-col">
-          {" "}
-          <img src={Oftalmologia} alt="" className=" w-40 " />{" "}
-          <h1>Oftalmologia</h1>
+        <div className=" bg-[#A2AF9B] h-60 w-full text-white flex items-center justify-center rounded-lg flex-col">
+          <img src={Oftalmologia} alt="" className="w-30 md:w-32 lg:w-40 " />
+          <h1 className="text-lg md:text-2xl lg:text-3xl">Oftalmologia</h1>
         </div>
       </section>
 
+      
       <section className="  flex justify-around">
-  <div className="bg-[#A2AF9B] mx-5 mt-5 p-7 h-180 w- text-3xl text-white flex items-center justify-center rounded-lg flex-col">
-    <h1 className="mb-9">Acerca de nosotros</h1>
-    <h2 className="descripcion">
-      Contamos con experiencia de mas de 10 años.
-      Brindamos atención médica integral con un enfoque humano, profesional y
-      cercano. Nuestro equipo de especialistas trabaja con compromiso y
-      tecnología actual para cuidar tu salud y la de tu familia, priorizando
-      siempre la confianza, la calidez y el bienestar.
-    </h2>
-    <img src={Imagen} alt="" className=" w-150 rounded-lg mt-6" />{" "}
-  </div>
-</section>;
+        <div className="bg-[#A2AF9B] mx-5 mt-5 p-7 h-180 w- text-3xl text-white flex items-center justify-center rounded-lg flex-col">
+          <h1 className="mb-9">Acerca de nosotros</h1>
+          <h2 className="descripcion">
+            Contamos con experiencia de mas de 10 años. Brindamos atención
+            médica integral con un enfoque humano, profesional y cercano.
+            Nuestro equipo de especialistas trabaja con compromiso y tecnología
+            actual para cuidar tu salud y la de tu familia, priorizando siempre
+            la confianza, la calidez y el bienestar.
+          </h2>
+          <img src={Imagen} alt="" className=" w-150 rounded-lg mt-6" />{" "}
+        </div>
+      </section>
+      ;
     </>
-
-
   );
 }
-
-
 
 export default Home;
